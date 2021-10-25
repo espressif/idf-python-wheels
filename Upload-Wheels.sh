aa8a04d0c74adafd0d3fe6c276d124bbab6367d2
Juraj Sadel <juraj.sadel@espressif.com>
2021-10-25 16:23:04 +0200

    add uploading to dl server

---
+++ Upload-Wheels.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd download
+
+for wfile in *.whl
+          do 
+            IFS=- read file_name  var1 <<< $wfile
+            file_name=$(echo "$file_name" | tr '[:upper:]' '[:lower:]')
+            aws s3 cp --acl=public-read --no-progress $wfile s3://$1/pypi/$file_name/$wfile
+done
---
+++ create_index_pages.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+import re
+from io import BytesIO
+from pathlib import Path
+
+import boto3
+
+HTML_HEADER = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="pypi:repository-version" content="1.0">
+    <title>Simple index</title>
+  </head>
+  <body>
+'''
+
+HTML_FOOTER = '''
+</body>
+</html>
+'''
+
+#DL_BUCKET = os.environ['DL_BUCKET']
+DL_BUCKET =  sys.argv[1]
+
+s3 = boto3.client('s3')
+
+paginator = s3.get_paginator('list_objects_v2')
+
+response_iterator = paginator.paginate(
+    Bucket=DL_BUCKET 
+    #,Prefix="pypi/"
+)
+
+
+packages = {}
+for response in response_iterator:
+    for package in response['Contents']:
+        res = re.search(r"(?<=\/)[a-zA-Z_\d]+(?=\-)", format(package['Key']))
+        #continue when package == index.html
+        if not res:
+            continue
+
+        name = res.group(0).lower()
+        if name not in packages:
+            packages[name] = []
+
+        packages[name].append(Path(package['Key']).name)
+        
+
+index = []
+index.append(HTML_HEADER)
+for name in packages.keys():
+    index.append(f'<a href="/pypi/{name}/">{name}/</a>')
+index.append(HTML_FOOTER)
+
+s3.upload_fileobj(BytesIO("\n".join(index).encode('utf-8')), DL_BUCKET, 'pypi/index.html', ExtraArgs={'ACL': 'public-read', 'ContentType':'text/html'})
+
+for name, filenames in packages.items():
+    index = []
+    index.append(HTML_HEADER)
+    for fn in filenames:
+        index.append(f'<a href="/pypi/{name}/{fn}">{fn}</a>')
+    index.append(HTML_FOOTER)
+
+    s3.upload_fileobj(BytesIO("\n".join(index).encode('utf-8')), DL_BUCKET, f'pypi/{name}/index.html', ExtraArgs={'ACL': 'public-read', 'ContentType':'text/html'})
