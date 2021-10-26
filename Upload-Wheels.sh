f99f4169b6e2e238152fad02a2ec8305b3fad3ac
Juraj Sadel <juraj.sadel@espressif.com>
2021-10-26 11:35:57 +0200

    fix Upload-Wheels command not found

--- Upload-Wheels.sh
+++ Upload-Wheels.sh
@@ -6,6 +6,5 @@ for wfile in *.whl
           do 
             IFS=- read file_name  var1 <<< $wfile
             file_name=$(echo "$file_name" | tr '[:upper:]' '[:lower:]')
-            echo $file_name
             aws s3 cp --acl=public-read --no-progress $wfile s3://$1/pypi/$file_name/$wfile
 done
