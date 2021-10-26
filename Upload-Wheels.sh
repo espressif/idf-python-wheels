81c5a97f6cd5192b5efb7a836511fbd78d2d5fc1
Juraj Sadel <juraj.sadel@espressif.com>
2021-10-26 10:58:07 +0200

    adding execute permissions

--- Upload-Wheels.sh
+++ Upload-Wheels.sh
@@ -1,8 +1,11 @@
+#! /bin/bash
+
 cd download
 
-for wfile in *.whl
+for wfile in download/*.whl
           do 
             IFS=- read file_name  var1 <<< $wfile
             file_name=$(echo "$file_name" | tr '[:upper:]' '[:lower:]')
+            echo $file_name
             aws s3 cp --acl=public-read --no-progress $wfile s3://$1/pypi/$file_name/$wfile
 done
