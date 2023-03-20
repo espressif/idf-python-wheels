1e5e3aa18d160e0d70b7432abf70e21aeedcb6e7
Juraj Sadel <juraj.sadel@espressif.com>
2023-03-20 15:04:11 +0100

    CI: don't uppload epstool wheels on Windows

--- Upload-Wheels.sh
+++ Upload-Wheels.sh
@@ -1,10 +1,15 @@
-#! /bin/bash
-
-cd download
-
 for wfile in *.whl
           do
             IFS=- read file_name  var1 <<< $wfile
             file_name=$(echo "$file_name" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+            case "$(uname -sr)" in
+              CYGWIN*|MINGW*|MINGW32*|MSYS*)
+                if [[ "$file_name" == "esptool" ]]; then
+                  continue
+                fi
+                ;;
+              *)
+                ;;
+            esac
             aws s3 cp --acl=public-read --no-progress $wfile s3://$1/pypi/$file_name/$wfile
 done
