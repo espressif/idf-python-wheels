d3d40e1bdc9df820f63087f4aacba8134e6ac3e5
Juraj Sadel <juraj.sadel@espressif.com>
2023-03-21 08:34:39 +0100

    CI: update upload wheels bash script and use it in macOS workflows

--- Upload-Wheels.sh
+++ Upload-Wheels.sh
@@ -1,19 +1,17 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-cd download
+cd download || exit
 
-for wfile in *.whl
-          do
-            IFS=- read file_name  var1 <<< $wfile
-            file_name=$(echo "$file_name" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
-            case "$(uname -sr)" in
-              CYGWIN*|MINGW*|MINGW32*|MSYS*)
-                if [[ "$file_name" == "esptool" ]]; then
-                  continue
-                fi
-                ;;
-              *)
-                ;;
-            esac
-            aws s3 cp --acl=public-read --no-progress $wfile s3://$1/pypi/$file_name/$wfile
+for wfile in *.whl; do
+  file_name=$(echo "${wfile%%-*}" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+  case "$(uname -sr)" in
+    CYGWIN*|MINGW*|MINGW32*|MSYS*)
+      if [[ "$file_name" == "esptool" ]]; then
+        continue
+      fi
+      ;;
+    *)
+      ;;
+  esac
+  aws s3 cp --acl=public-read --no-progress "$wfile" "s3://$1/pypi/$file_name/$wfile"
 done
