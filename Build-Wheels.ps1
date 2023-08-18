espressif/idf-python-wheels

commit eaf1cc85621c78ae5316e44d1a924716839418c1

feat(build): Add initial support for custom build environment
The build environment is used to fix gevent build caused by
Cython==3.0.0 breaking changes.

Build-Wheels.ps1
@@ -4,6 +4,7 @@ param (
     [string]$Branch="main",
     [string]$Python="python3",
     [string]$Arch="",
+    [string[]]$BuildEnv=@(),
     [string[]]$CompileWheels=@(),
     [switch]$NoReq=$false
 )
@@ -29,6 +30,23 @@ $OnlyBinary = ""
 "Processing: $RequirementsUrl"
 Invoke-WebRequest $RequirementsUrl -OutFile $RequirementsTxt
 
+# If specific build environment is requested then build isolation must be disable in order to use the build environment.
+# It might be necessary to clean or separate this build environment in the future.
+if ($BuildEnv.count -eq 0) {
+    $ExtraPipArgs = ""
+} else {
+    $ExtraPipArgs = "--no-build-isolation"
+}
+
+# Install packages requested for the build environment
+foreach ($build_req in $BuildEnv) {
+    if ("$Arch" -eq "") {
+        &$Python -m pip install $build_req
+    } else {
+        arch $Arch $Python -m pip install $build_req
+    }
+}
+
 # Iterate over binaries which should be compiled.
 # The build of next binary will receive list of previously build of binaries to avoid download
 foreach ($wheelPrefix in $CompileWheels) {
@@ -50,9 +68,9 @@ foreach ($wheelPrefix in $CompileWheels) {
     # "   ".Split() vs. "   ".split() | where {$_}
     $OnlyBinarySplitted = $OnlyBinary.Split(' ') | where {$_}
     if ("$Arch" -eq "") {
-        &$Python -m pip wheel --find-links download --wheel-dir download $OnlyBinarySplitted $wheel
+        &$Python -m pip wheel --find-links download --wheel-dir download $ExtraPipArgs $OnlyBinarySplitted $wheel
     } else {
-        arch $Arch $Python -m pip wheel --find-links download --wheel-dir download $OnlyBinarySplitted $wheel
+        arch $Arch $Python -m pip wheel --find-links download --wheel-dir download $ExtraPipArgs $OnlyBinarySplitted $wheel
     }
     #$cache=pip cache dir
     #Get-ChildItem -Path $cache "{$wheel}*.whl" -Recurse | % {Copy-Item -Path $_.FullName -Destination download -Container }

.github/workflows/build-wheels-linux-arm64-self-hosted.yml
@@ -86,7 +86,10 @@ jobs:
           if ( "${{ inputs.packages }}" ) {
             .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -NoReq -CompileWheels @(${{ env.packages }})
           } else {
-            .\Build-Wheels.ps1 -Branch ${{ env.IDF_branch }} -CompileWheels @("greenlet==1.0.0", "gevent==1.5.0", "cryptography", "python-pkcs11")
+            # Cython==3.0.0 breaks the build of gevent. The build environment from https://github.com/gevent/gevent/blob/1.5.0/pyproject.toml
+            # must be patched with "cython<3"
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -BuildEnv @("setuptools", "wheel", "cython<3", "cffi") -CompileWheels @("gevent==1.5.0")
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -CompileWheels @("cryptography", "python-pkcs11")
           }
       - name: Copy cached wheels
         run: cp -u `pip cache list --format=abspath` download

.github/workflows/build-wheels-linux-armv7-self-hosted.yml
@@ -90,7 +90,10 @@ jobs:
           if ( "${{ inputs.packages }}" ) {
             .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -NoReq -CompileWheels @(${{ env.packages }})
           } else {
-            .\Build-Wheels.ps1 -Branch ${{ env.IDF_branch }} -CompileWheels @("greenlet==1.0.0", "gevent==1.5.0", "cryptography", "python-pkcs11")
+            # Cython==3.0.0 breaks the build of gevent. The build environment from https://github.com/gevent/gevent/blob/1.5.0/pyproject.toml
+            # must be patched with "cython<3"
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -BuildEnv @("setuptools", "wheel", "cython<3", "cffi") -CompileWheels @("gevent==1.5.0")
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -CompileWheels @("cryptography", "python-pkcs11")
           }
       - name: Copy cached wheels
         run: cp -u `pip cache list --format=abspath` download

.github/workflows/build-wheels-macos-M1-self-hosted.yml
@@ -108,7 +108,10 @@ jobs:
           if ( "${{ inputs.packages }}" ) {
             .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -NoReq -CompileWheels @(${{ env.packages }})
           } else {
-            .\Build-Wheels.ps1 -Branch ${{ env.IDF_branch }} -Arch "-arm64" -CompileWheels @("greenlet==1.0.0", "gevent==1.5.0", "cryptography", "python-pkcs11") -Python ~/.pyenv/versions/${{ matrix.python-version }}/bin/python
+            # Cython==3.0.0 breaks the build of gevent. The build environment from https://github.com/gevent/gevent/blob/1.5.0/pyproject.toml
+            # must be patched with "cython<3"
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -BuildEnv @("setuptools", "wheel", "cython<3", "cffi") -CompileWheels @("gevent==1.5.0") -Python ~/.pyenv/versions/${{ matrix.python-version }}/bin/python
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -CompileWheels @("cryptography", "python-pkcs11") -Python ~/.pyenv/versions/${{ matrix.python-version }}/bin/python
           }
       - name: Copy cached wheels
         run: |

.github/workflows/build-wheels-macos-dispatch.yml
@@ -90,7 +90,10 @@ jobs:
           if ( "${{ inputs.packages }}" ) {
             .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -Arch "${{ matrix.ARCH }}" -NoReq -CompileWheels @(${{ env.packages }})
           } else {
-            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -Arch "${{ matrix.ARCH }}" -CompileWheels @("greenlet==1.0.0", "gevent==1.5.0", "cryptography", "python-pkcs11")
+            # Cython==3.0.0 breaks the build of gevent. The build environment from https://github.com/gevent/gevent/blob/1.5.0/pyproject.toml
+            # must be patched with "cython<3"
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -BuildEnv @("setuptools", "wheel", "cython<3", "cffi") -CompileWheels @("gevent==1.5.0")
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -CompileWheels @("cryptography", "python-pkcs11")
           }
       - name: Copy cached wheels
         run: |

.github/workflows/build-wheels-ubuntu-dispatch.yml
@@ -94,7 +94,10 @@ jobs:
           if ( "${{ inputs.packages }}" ) {
             .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -NoReq -CompileWheels @(${{ env.packages }})
           } else {
-            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -CompileWheels @("greenlet==1.0.0", "gevent==1.5.0", "cryptography", "python-pkcs11")
+            # Cython==3.0.0 breaks the build of gevent. The build environment from https://github.com/gevent/gevent/blob/1.5.0/pyproject.toml
+            # must be patched with "cython<3"
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -BuildEnv @("setuptools", "wheel", "cython<3", "cffi") -CompileWheels @("gevent==1.5.0")
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -CompileWheels @("cryptography", "python-pkcs11")
           }
       - name: Test wheels by installation
         shell: pwsh

.github/workflows/build-wheels-windows-dispatch.yml
@@ -95,7 +95,10 @@ jobs:
           if ( "${{ inputs.packages }}" ) {
             .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -Arch "${{ matrix.ARCH }}" -NoReq -CompileWheels @(${{ env.packages }})
           } else {
-            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -Arch "${{ matrix.ARCH }}" -CompileWheels @("greenlet==1.0.0", "gevent==1.5.0", "cryptography", "windows-curses", "python-pkcs11")
+            # Cython==3.0.0 breaks the build of gevent. The build environment from https://github.com/gevent/gevent/blob/1.5.0/pyproject.toml
+            # must be patched with "cython<3"
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -BuildEnv @("setuptools", "wheel", "cython<3", "cffi") -CompileWheels @("gevent==1.5.0")
+            .\Build-Wheels.ps1  -Branch ${{ env.IDF_branch }} -CompileWheels @("cryptography", "windows-curses", "python-pkcs11")
           }
       - name: Test wheels by installation
         shell: pwsh

commit 3ae79e1a0af5c78da9c7044cd6bb07b99fa08296

fix: don't set $ExtraPipArgs to empty string
It seems that this gets passed to pip-wheels as a positional argument
and resulting in "ERROR: Invalid requirement: ''". Since we are not
checking return values from the pip command, this is silently ignored.
Not setting it at all(keeping the $null), in case where the $BuildEnv
is empty, seem to be working.

Fixes: eaf1cc85621c ("feat(build): Add initial support for custom build env..")

Signed-off-by: Frantisek Hrbata <frantisek.hrbata@espressif.com>

Build-Wheels.ps1
@@ -32,9 +32,7 @@ Invoke-WebRequest $RequirementsUrl -OutFile $RequirementsTxt
 
 # If specific build environment is requested then build isolation must be disable in order to use the build environment.
 # It might be necessary to clean or separate this build environment in the future.
-if ($BuildEnv.count -eq 0) {
-    $ExtraPipArgs = ""
-} else {
+if ($BuildEnv.count -ne 0) {
     $ExtraPipArgs = "--no-build-isolation"
 }
 