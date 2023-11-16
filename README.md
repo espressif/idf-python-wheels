dd6d7892e1fc966e46745f8234a84ebc67eaebfb
Jakub Kocka <jakub.kocka@espressif.com>
2023-11-16 07:40:23 +0100

    feat(build_wheels): Added include_list and exclude_list

--- README.md
+++ README.md
@@ -43,11 +43,44 @@ The build contains all wheels required by branches:
 ## Requirements lists
 These lists are files for requirements that should be added or excluded from the main requirements list which is automatically assembled.
 
-### include_list
-File for additional Python packages to the main requirements list. Built separately to not restrict the main requirements list.
+### exclude_list.yaml
+File for excluded Python packages in the **main requirements** list.
 
-### exclude_list
-File for excluded Python packages in the main requirements list.
+This YAML file is converted to Requirement from packaging.requirements because pip can handle this format, so the function for converting is designed to be compatible with [PEP508](https://peps.python.org/pep-0508/) scheme.
+The opposite logic of exclude_list is handled by the function itself, which means it is supposed to be easy to use for developers, this is also the reason YAML format is used.
+
+For every `package_name` there are options:
+* `version` - supports all logic operators defined by PEP508 for versions (<, >, !=, etc.)
+* `platform`
+
+which could be a string or a list of strings.
+
+exclude_list template:
+
+    - package_name: '<name_of_package>'
+        version: '<package_version_with_operator>' / ['<package_version_with_operator>', '<package_version_with_operator>']     # optional
+        platform: '<platform>' / ['<platform>', '<platform>', '<platform>']                                                     # optional
+
+The syntax can be converted into a sentence: "From assembled **main requirements** exclude `package_name` with `version` on `platform`".
+
+example:
+
+    - package_name: 'pyserial'
+        version: ['>=3.3', '<3.6']
+        platform: ['win32', 'linux', 'darwin']
+
+This would mean: "From assembled **main requirements** exclude `pyserial` with version `>=3.3` and `<3.6` on platform `win32`, `linux`, `darwin`".
+
+From the example above is clear that the `platform` could be left out (because all main platforms are specified) so the options `platform` or `version` are optional, one of them or both can be not specified and the key can be erased. When only `package_name` is given the package will be excluded from **main requirements**.
+
+
+### include_list.yaml
+File for additional Python packages to the **main requirements** list. Built separately to not restrict the **main requirements** list.
+
+This YAML file uses the same mechanism such as **exclude_list** but without the opposite logic.
+
+The syntax can be also converted into a sentence: "For assembled **main requirements** additionally include `package_name` with `version` on `platform`".
+
 
 ### build_requirements.txt
 File for the requirements needed for the build process and the build script.
--- build_wheels.py
+++ build_wheels.py
@@ -1,11 +1,15 @@
 import json
 import os
 import re
+import subprocess
 from typing import List
 from typing import Optional
 from typing import Union
 
 import requests
+import yaml
+from colorama import Fore
+from colorama import Style
 from packaging.requirements import InvalidRequirement
 from packaging.requirements import Requirement
 
@@ -117,7 +121,7 @@ def get_constraints_versions(idf_branches: List[str]) -> List[str]:
 # --- Download all requirements from all the branches requirements and constraints files --- #
 def _download_branch_requirements(branch: str, idf_requirements_json: dict) -> List[str]:
     """Download requirements files for all groups specified in IDF requirements.JSON"""
-    print(f'---------- ESP-IDF BRANCH {branch} ----------')
+    print(Fore.BLUE + f'---------- ESP-IDF BRANCH {branch} ----------' + Style.RESET_ALL)
     requirements_txt: List[str] = []
 
     for feature in idf_requirements_json['features']:
@@ -138,6 +142,7 @@ def _download_branch_constraints(constraint_file_url: str, branch, idf_constrain
     return []
 
 
+non_classic_requirement:List[str] = []
 def _add_into_requirements(requirements_txt: List[str]) -> set:
     """Create set of requirements from downloaded lines of requirements
         - set is used to prevent duplicates
@@ -150,12 +155,12 @@ def _add_into_requirements(requirements_txt: List[str]) -> set:
             try:
                 requirements_set.add(Requirement(line))
             except InvalidRequirement:
-                # TODO not a classic requirement (e.g. '--only-binary cryptography') when building wheels
-                requirements_set.add(line)
+                # Non classic requirement (e.g. '--only-binary cryptography')
+                non_classic_requirement.append(line)
     return requirements_set
 
 
-def assemble_requirements(idf_branches: List[str], idf_constraints: List[str]) -> set:
+def assemble_requirements(idf_branches: List[str], idf_constraints: List[str], make_txt_file:bool=False) -> set:
     """Assemble IDF requirements into set to prevent duplicates"""
     requirements_txt: List[str] = []
 
@@ -172,18 +177,176 @@ def assemble_requirements(idf_branches: List[str], idf_constraints: List[str]) -
         requirements_txt += _download_branch_requirements(branch, idf_requirements_json)
         requirements_txt += _download_branch_constraints(constraint_file_url, branch, idf_constraints[i])
 
+    if make_txt_file:
+        # TXT file from all downloaded requirements and constraints files
+        # useful for debugging or to see the comments for requirements
+        with open('requirements.txt', 'w') as f:
+            for line in requirements_txt:
+                f.write(line)
+                f.write('\n')
+
     return _add_into_requirements(requirements_txt)
 
 
-def print_requirements(requirements_set: set):
-    """Prints assembled list of requirements"""
-    print('\n---------- REQUIREMENTS ----------')
-    for req in requirements_set:
-        print(req)
+# --- exclude_list and include_list ---
+def _change_specifier_logic(specifier: str) -> str:
+    """Change specifier logic to opposite
+        - e.g. "<3" will be ">3"
+    """
+    #print(f"Original specifier: {specifier}")
+    new_spec = specifier
+    replacements = {'<': '>',
+                    '>': '<',
+                    '!': '=',
+                    '==': '!='}
+    for old, new in replacements.items():
+        if old in specifier and '===' not in specifier:
+            new_spec = specifier.replace(old, new)
+    #print(f"Specifier changed: {new_spec}")
+    return new_spec
+
+
+def yaml_to_requirement(yaml_file:str, exclude: bool = False) -> set:
+    """Converts YAML defined package into packaging.requirements Requirement
+    which can be directly used with pip
+    - when exclude is set to True it makes opposite logic for Requirement to be excluded"""
+    with open(yaml_file, 'r') as f:
+        yaml_list = yaml.load(f, yaml.Loader)
+
+    exclude_list_set: set[Requirement] = set()
+
+    if not yaml_list:
+        return exclude_list_set
+
+    for package in yaml_list:
+        req_txt = f"{package['package_name']}"
+
+        if 'version' in package:
+            if isinstance(package['version'], list):
+                ver_txt = ''
+                for ver_str in package['version']:
+                    if exclude:
+                        ver_txt += f'{_change_specifier_logic(ver_str)},'
+                    else:
+                        ver_txt += f'{ver_str},'
+                req_txt += ver_txt[:-1]
+            else:
+                if exclude:
+                    req_txt += _change_specifier_logic(package['version'])
+                else:
+                    req_txt += package['version']
+
+        if 'platform' in package:
+            if isinstance(package['platform'], list):
+                plf_txt = '; '
+                for plf_str in package['platform']:
+                    if exclude:
+                        plf_txt += f"sys_platform != '{plf_str}' or "
+                    else:
+                        plf_txt += f"sys_platform == '{plf_str}' or "
+                req_txt += plf_txt[:-4]
+            else:
+                if exclude:
+                    req_txt += f"; sys_platform != '{package['platform']}'"
+                else:
+                    req_txt += f"; sys_platform == '{package['platform']}'"
+
+        exclude_list_set.add(Requirement(req_txt))
+    return exclude_list_set
+
+
+def exclude_from_requirements(assembled_requirements:set, exclude_list: set, print_requirements: bool = True) -> set:
+    """Exclude packages defined in exclude_list from assembled requirements
+        - print_requirements = true will print the changes
+    """
+    new_assembled_requirements = set()
+    not_in_exclude = []
+    if print_requirements:
+        print(Fore.BLUE + '---------- REQUIREMENTS ----------')
+
+    for requirement in assembled_requirements:
+        for req in exclude_list:
+            if req.name in requirement.name:
+
+                if requirement.specifier and req.specifier:
+                    new_specifier = requirement.specifier & req.specifier
+                elif requirement.specifier and not req.specifier:
+                    new_specifier = requirement.specifier
+                elif not requirement.specifier and req.specifier:
+                    new_specifier = req.specifier
+                else:
+                    new_specifier = ''
+
+                if requirement.marker and req.marker:
+                    new_markers = f'({requirement.marker}) and ({req.marker})'
+                elif requirement.marker and not req.marker:
+                    new_markers = requirement.marker
+                elif not requirement.marker and req.marker:
+                    new_markers = req.marker
+                else:
+                    new_markers = ''
+
+                if not req.specifier and not req.marker:
+                    if print_requirements:
+                        print(Fore.RED + f'-- {requirement}')
+                    continue
+
+                if new_markers:
+                    new_requirement = Requirement(f'{requirement.name}{new_specifier}; {new_markers}')
+                else:
+                    new_requirement = Requirement(f'{requirement.name}{new_specifier}')
+
+                new_assembled_requirements.add(new_requirement)
+
+                if print_requirements:
+                    print(Fore.RED + f'-- {requirement}')
+                    print(Fore.GREEN + f'++ {new_requirement}')
+            else:
+                not_in_exclude.append(True)
+        if len(not_in_exclude) == len(exclude_list):
+            if print_requirements:
+                print(Style.RESET_ALL + str(requirement))
+            new_assembled_requirements.add(requirement)
+
+        not_in_exclude = []
+
+    if print_requirements:
+        print(Fore.BLUE + '---------- END OF REQUIREMENTS ----------' + Style.RESET_ALL)
+
+    return new_assembled_requirements
+
+
+# --- Build wheels ---
+def build_wheels(requirements: set):
+    """Build Python wheels"""
+    dir = f'{os.path.curdir}{(os.sep)}downloaded_wheels'
+    for requirement in requirements:
+        # --only-binary requirement wheel build
+        if non_classic_requirement and non_classic_requirement[0].replace('--only-binary ', '') in requirement.name:
+            only_bin = non_classic_requirement[0].replace('--only-binary', '').strip()
+            var = subprocess.run(
+                ['python', '-m', 'pip', 'wheel', f'{requirement}', '--find-links', f'{dir}',
+                 '--wheel-dir', f'{dir}', '--only-binary', f'{only_bin}'],
+                 stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+            print(f'Ret_code: {str(var.returncode)}, std_err: {str(var.stderr)}, std_out: {str(var.stdout)}')
+
+            non_classic_requirement.remove(non_classic_requirement[0])
+            continue
+
+        # requirement wheel build
+        var = subprocess.run(
+            ['python', '-m', 'pip', 'wheel', f'{requirement}', '--find-links', f'{dir}',
+             '--wheel-dir', f'{dir}'],
+             stdout=subprocess.PIPE, stderr=subprocess.PIPE
+             )
+        print(f'Ret_code: {str(var.returncode)}, std_err: {str(var.stderr)}, std_out: {str(var.stdout)}')
 
 
 def main():
-    """Builds Python wheels for IDF dependencies"""
+    """Builds Python wheels for ESP-IDF dependencies for master and release branches
+    grater or equal to specified"""
+
     idf_repo_branches = fetch_idf_branches()
     idf_branches = get_used_idf_branches(idf_repo_branches)
     print(f'ESP-IDF branches to be downloaded requirements for:\n{idf_branches}\n')
@@ -191,9 +354,20 @@ def main():
     idf_constraints = get_constraints_versions(idf_branches)
     print(f'ESP-IDF constrains files versions to be downloaded requirements for:\n{idf_constraints}\n')
 
-    requirements_set = assemble_requirements(idf_branches, idf_constraints)
+    requirements = assemble_requirements(idf_branches, idf_constraints)
 
-    print_requirements(requirements_set)
+    exclude_list = yaml_to_requirement('exclude_list.yaml', exclude=True)
+    excluded_requirements = exclude_from_requirements(requirements, exclude_list)
+
+    include_list = yaml_to_requirement('include_list.yaml')
+    print(Fore.BLUE + '---------- ADDITIONAL REQUIREMENTS ----------' + Style.RESET_ALL)
+    for req in include_list:
+        print(req)
+    print(Fore.BLUE + '---------- END OF ADDITIONAL REQUIREMENTS ----------' + Style.RESET_ALL)
+
+    build_wheels(excluded_requirements)
+
+    build_wheels(include_list)
 
 
 if __name__ == '__main__':
