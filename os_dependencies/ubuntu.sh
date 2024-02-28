5eb9b0f81021eda68cb29ac04d3bea28232905c6
Jakub Kocka <jakub.kocka@espressif.com>
2024-02-28 10:33:39 +0100

    fix(ubuntu_dispatch): Fetch of some archives fails

--- os_dependencies/ubuntu.sh
+++ os_dependencies/ubuntu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt-get update -y
+
 # PyGObject needs build dependecies https://pygobject.readthedocs.io/en/latest/getting_started.html
 sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0 -y
 
