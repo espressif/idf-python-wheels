3164d731cfeb718d3ead75f2d86700452de928f8
Frantisek Hrbata <frantisek.hrbata@espressif.com>
2025-03-17 15:52:26 +0100

    fix: add new dependencies for pygobject
    https://pygobject.gnome.org/getting_started.html#ubuntu-logo-ubuntu-debian-logo-debian
    
    pygobject now requires libgirepository-2.0-dev
    
    Signed-off-by: Frantisek Hrbata <frantisek.hrbata@espressif.com>

--- os_dependencies/ubuntu.sh
+++ os_dependencies/ubuntu.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y
 
 # PyGObject needs build dependecies https://pygobject.readthedocs.io/en/latest/getting_started.html
-sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0 -y
+sudo apt install libgirepository1.0-dev libgirepository-2.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0 -y
 
 # dbus-python needs build dependecies
 sudo apt-get install cmake build-essential libdbus-1-dev libdbus-glib-1-dev -y
